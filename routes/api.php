c1277cc97e163fc556a0b14e5a22b161611105ef
arifnugrahaa <arifnugraha1243@gmail.com>
2022-06-19 16:30:25 +0700

    endpoint menampilkan daftar quotes

--- routes/api.php
+++ routes/api.php
@@ -21,4 +21,5 @@ Route::post('register', 'Api\RegisterController@action');
 Route::post('login', 'Api\LoginController@action');
 Route::get('me', 'Api\UserController@me')->middleware('auth:api');
 Route::post('quote', 'Api\QuoteController@store')->middleware('auth:api');
+Route::get('quote', 'Api\QuoteController@index')->middleware('auth:api');
 
