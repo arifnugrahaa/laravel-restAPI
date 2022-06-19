772e8bff898090ec1a2b33aa708bf5ee04439c0e
arifnugrahaa <arifnugraha1243@gmail.com>
2022-06-19 15:25:04 +0700

    membuat endpoint login

--- routes/api.php
+++ routes/api.php
@@ -18,3 +18,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::post('register', 'Api\RegisterController@action');
+Route::post('login', 'Api\LoginController@action');
+
